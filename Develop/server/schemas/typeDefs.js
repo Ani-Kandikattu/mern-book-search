f733f66505c935d0691f6a5b45bc1f109d26cd4d
Ani-Kandikattu <120747994+Ani-Kandikattu@users.noreply.github.com>
2023-07-06 20:54:24 -0700

    created schemas folder and began adding resolvers and typeDefs using the class materials for reference

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,17 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
