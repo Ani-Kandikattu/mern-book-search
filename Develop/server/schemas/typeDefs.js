b3879a75b97c89862a7050cdc48c13ede86df2f0
Ani-Kandikattu <anikandikattu@gmail.com>
2023-07-07 19:47:20 -0700

    finished typeDefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -8,9 +8,34 @@ const typeDefs = gql`
     bookCount: Int
     savedBooks: [Book]
   }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
   type Auth {
     token: ID!
     user: User
+  }
+  input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
